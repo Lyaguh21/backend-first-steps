f590262382eb861144d474b6ad150611d0977099
Lyaguh NoteBook <artyom07206@gmail.com>
2026-02-22 12:06:26 +0300

    nest init

--- index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app");
-require("dotenv").config();
-
-const PORT = Number(process.env.PORT) || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
