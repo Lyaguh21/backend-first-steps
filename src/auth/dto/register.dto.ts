06b7c26ff333ed226d91bb9f08c78035c34eaffd
LyaguhPC <artyom07206@gmail.com>
2026-02-26 22:41:22 +0300

    swaga 2.0

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty({ example: 'user@example.com' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: 'SupePassword123' })
   @IsString()
   @MinLength(4)
   password: string;
--- src/users/dto/update-role.dto.ts
+++ src/users/dto/update-role.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsIn, IsInt } from 'class-validator';
 import type { Role } from 'src/types/auth-user.type';
 
 export class UpdateRoleDto {
+  @ApiProperty({ example: 2 })
   @IsInt()
   userId: number;
 
+  @ApiProperty({ example: 'COOK' })
   @IsIn(['ADMIN', 'WAITER', 'COOK', 'CUSTOMER'])
   role: Role;
 }
